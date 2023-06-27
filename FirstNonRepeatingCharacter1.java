6c623bc684375ee5ea3b5aebad06404a163106a3
shivampw <123968519+shivampw@users.noreply.github.com>
2023-06-27 18:27:20 +0530

    Add files via upload

---
+++ FirstNonRepeatingCharacter1.java
@@ -0,0 +1,32 @@
+ // First Unique Character in a String Example 2
+
+ import java.util.HashMap;
+ import java.util.Map;
+ 
+ public class FirstNonRepeatingCharacter1 {
+     public static void main(String[] args) {
+                 String s = "loveleetcode";
+                 int index = firstUniqChar(s);
+                 System.out.println("first non-repeating character: " + index);
+             }
+ 
+             public static int firstUniqChar(String s) {
+                 Map<Character, Integer> charCountMap = new HashMap<>();
+ 
+                 // Count the occurrences of each character in the string
+                 for (char c : s.toCharArray()) {
+                     charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+                 }
+ 
+                 // Find the index of the first non-repeating character
+                 for (int i = 0; i < s.length(); i++) {
+                     if (charCountMap.get(s.charAt(i)) == 1) {
+                         return i;
+                     }
+                 }
+ 
+                 // If no non-repeating character is found, return -1
+                 return -1;
+             }
+     
+ }
